8529fa3c0c4839ceac62fd6a9d7176ea5b134786
N3nT <bar55v71@interia.pl>
2025-02-03 13:24:07 +0100

    Fix marker on live version

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,22 +1,31 @@
 import { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: '../node_modules/leaflet/dist/images/marker-icon-2x.png',
+    iconUrl: '../node_modules/leaflet/dist/images/marker-icon.png',
+    shadowUrl: '../node_modules/leaflet/dist/images/marker-shadow.png'
+});
 
 function ChangeView({ center, zoom }) {
-	const map = useMap();
-  
+	const map = useMap()
+
 	useEffect(() => {
-	  map.setView(center, zoom);
-	}, [center, zoom, map]);
-  
-	return null;
-  }
+		map.setView(center, zoom)
+	}, [center, zoom, map])
+
+	return null
+}
 
 const MapComponent = (props) => {
-	const [cords, setCords] = useState({x: props.x, y: props.y})
+	const [cords, setCords] = useState({ x: props.x, y: props.y })
 	useEffect(() => {
-		setCords({x: props.x, y: props.y});
-	}, [props.x, props.y]);
+		setCords({ x: props.x, y: props.y })
+	}, [props.x, props.y])
 
 	return (
 		<MapContainer
@@ -28,7 +37,7 @@ const MapComponent = (props) => {
 			attributionControl={false}
 			className='fixed'
 		>
-		<ChangeView center={[cords.x, cords.y]} zoom={9} />
+			<ChangeView center={[cords.x, cords.y]} zoom={9} />
 			<TileLayer
 				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
